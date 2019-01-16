3579180719e5d376d092cfc77976f4e366b1c852
Austin Moran <a.moran3257@gmail.com>
2019-01-16 11:49:34 -0700

    basic server implemented

--- app.js
+++ app.js
@@ -7,4 +7,24 @@ const data = require('./data.json')
 app.use(express.static('public'))
 
 
-app.get()
+app.get('/', (req, res, next) => {
+  res.status(200)
+  res.send(data)
+})
+
+app.get('/:tag', (req, res, next) => {
+  let tag = req.params.tag
+  if(data.tags.includes(tag)) {
+    let filteredData = data.songs.filter(song => song.tags.includes(tag))
+    res.status(200)
+    res.send(filteredData)
+  } else {
+    res.status(404)
+    // res.send(`try one of these: ${data.tags}`)
+    res.send('try one of these:', data.tags)
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Party on port: ${port}`)
+})
